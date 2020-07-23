0d5d8c648dabad53c701b2c7a3c05053cd40f79e
Hadrien Croubois <hc@iex.ec>
2020-07-23 22:15:37 +0200

    adding Rest api

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 'use strict'
 
+const express     = require('express')
+const bodyParser  = require('body-parser')
 const IexecODBP2P = require('./src/iexecodbp2p')
+const liborders   = require('./src/utils/liborders')
 
 async function main()
 {
+	// IexecODBP2P
 	const instance = await IexecODBP2P.create({ listen: process.env.LISTEN })
 
+	// Rest API
+	const app = express()
+	app.use(bodyParser.json())
+	app.use(bodyParser.urlencoded({ extended: true }))
+
+	app.route('/order/:hash?')
+	.get(async (req, res) => {
+		const result = instance.datastore.db.get(req.params.hash)
+		result
+			? res.json({ result })
+			: res.json({ error: 'no entry found' })
+	})
+	.post(async (req, res) => {
+		try
+		{
+			const domain = liborders.clean(req.body.domain, false) // withSign: false
+			const order  = liborders.clean(req.body.order, true) // withSign: true
+			const hash   = liborders.hash(domain, order)
+			await instance.datastore.db.put({ hash, domain, order })
+			res.json({ result: true })
+		}
+		catch (error)
+		{
+			res.json({ error: error.message })
+		}
+	})
+
+	process.env.PORT && app.listen(process.env.PORT)
+
+	// Show current content
 	process.stdin.on('data', () => {
-		console.log('query:', instance.datastore.db.query((entry) => true))
+		const orders = instance.datastore.db.query((entry) => true)
+		console.log(`${orders.length} orders stored`)
+		orders.forEach(({ hash }) => console.log(`- ${hash} - `))
 	})
 }
 
